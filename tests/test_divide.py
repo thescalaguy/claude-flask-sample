304f864a7583b9ed851af51d00bb24a9f2e78cc4
Fasih Khatib <hellofasih.confound928@passinbox.com>
2026-02-17 12:36:37 +0530

    Refactor fixture into conftest.py

--- tests/test_divide.py
+++ tests/test_divide.py
@@ -1,16 +1,3 @@
-import pytest
-
-from app import create_app
-
-
-@pytest.fixture
-def client():
-    app = create_app()
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
-
-
 def test_divide_integers(client):
     response = client.post("/divide", json={"a": 10, "b": 2})
     assert response.status_code == 200
